82fb27385f42102879a6bfb414bdd718960d68f3
Sergii Zasenko <sergii@zasenko.name>
2022-08-22 00:42:52 +0300

    refactor day 19

--- adv19/src/test/java/name/zasenko/aoc21/day19/MapTest.java
+++ adv19/src/test/java/name/zasenko/aoc21/day19/MapTest.java
@@ -10,18 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MapTest {
-    ClassLoader classLoader;
+    List<Scanner> scanners;
 
     @BeforeEach
-    public void init() {
-        classLoader = getClass().getClassLoader();
+    void init() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream in = classLoader.getResourceAsStream("scanner1.txt");
+        scanners = ScannerFactory.fromStream(in);
     }
 
     @Test
     public void findScannerOffset() {
-        InputStream in = classLoader.getResourceAsStream("scanner1.txt");
-        List<Scanner> scanners = ScannerFactory.fromStream(in);
-
         Map map = new Map(scanners.get(0));
         Scanner transformedScanner = map.findOffset(scanners.get(1)).getRotatedShiftedScanner();
         assertArrayEquals(new int[]{-618, -824, -621}, transformedScanner.getCoords().get(0).getCoord());
@@ -29,8 +28,6 @@ public class MapTest {
 
     @Test
     public void revealWholeMapTest() {
-        InputStream in = classLoader.getResourceAsStream("scanner1.txt");
-        List<Scanner> scanners = ScannerFactory.fromStream(in);
         Map map = new Map(scanners);
 
         assertEquals(79, map.getCoords().size());
